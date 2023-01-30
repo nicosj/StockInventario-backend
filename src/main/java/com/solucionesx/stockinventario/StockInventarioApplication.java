650a611aa21d973e27c7a86c3cb28b6a608cfeae
Nicolas Rodriguez <nico.river09.9@gmail.com>
2023-01-30 16:00:03 -0300

    add: proyecto base de control de _Inventario

---
+++ src/main/java/com/solucionesx/stockinventario/StockInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.solucionesx.stockinventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockInventarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockInventarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solucionesx/stockinventario/StockInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solucionesx.stockinventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockInventarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
