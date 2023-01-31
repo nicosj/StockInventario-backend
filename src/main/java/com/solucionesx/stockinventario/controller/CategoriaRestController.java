997aa0df3812389ecfe87ab56dfdaf9aaea1e6f9
Nicolas Rodriguez <nico.river09.9@gmail.com>
2023-01-31 16:32:06 -0300

    add: Buscar por categoria por Id

--- src/main/java/com/solucionesx/stockinventario/controller/CategoriaRestController.java
+++ src/main/java/com/solucionesx/stockinventario/controller/CategoriaRestController.java
@@ -5,6 +5,7 @@ import com.solucionesx.stockinventario.services.ICategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,25 @@ public class CategoriaRestController {
     @Autowired
     private ICategoriaService service;
 
+    /**
+     * Busca todas las categorias
+     * @return
+     */
     @GetMapping("/categorias")
     public ResponseEntity<CategoriaResponseRest> searchCategoria(){
          ResponseEntity<CategoriaResponseRest> responseEntity = service.search();
          return responseEntity;
     }
-}
+
+    /**
+     * Busca una categoria por id
+     * @param id
+     * @return
+     */
+    @GetMapping("/categorias/{id}")
+    public ResponseEntity<CategoriaResponseRest> searchCategoriaById(@PathVariable Long id) {
+        ResponseEntity<CategoriaResponseRest> responseEntity = service.searchById(id);
+        return responseEntity;
+    }
+
+    }
--- src/main/java/com/solucionesx/stockinventario/services/CategoriaServiceImpl.java
+++ src/main/java/com/solucionesx/stockinventario/services/CategoriaServiceImpl.java
@@ -9,7 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoriaServiceImpl implements ICategoriaService{
@@ -27,6 +29,29 @@ public class CategoriaServiceImpl implements ICategoriaService{
 
         } catch (Exception e) {
             response.setMetadata("Error", "500", "Error al consultar las categorias");
+            e.getStackTrace();
+            return new ResponseEntity<CategoriaResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<CategoriaResponseRest>(response, HttpStatus.OK);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ResponseEntity<CategoriaResponseRest> searchById(Long id) {
+        CategoriaResponseRest response = new CategoriaResponseRest();
+        List<Categoria> categorias = new ArrayList<>();
+        try {
+            Optional<Categoria> categoria = categoriaDao.findById(id);
+            if(categoria.isPresent()){
+                categorias.add(categoria.get());
+                response.getData().setCategorias(categorias);
+                response.setMetadata("Respuesta ok", "200", "Categoria encontrada");
+            }else {
+                response.setMetadata("Sin resultado", "200", "Categoria no encontrada");
+                return new ResponseEntity<CategoriaResponseRest>(response, HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            response.setMetadata("Error", "500", "Error al consultar por id");
             e.getStackTrace();
             return new ResponseEntity<CategoriaResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
